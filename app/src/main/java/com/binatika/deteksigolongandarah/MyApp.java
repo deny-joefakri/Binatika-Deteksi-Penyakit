70a123f9e6580df03ad97270a7155f378134b5d2
denyjoefakri <denyjoefakri@gmail.com>
2017-06-02 14:31:26 +0700

    fix UI

--- app/src/main/java/com/binatika/deteksigolongandarah/MyApp.java
+++ app/src/main/java/com/binatika/deteksigolongandarah/MyApp.java
@@ -1,14 +1,31 @@
 package com.binatika.deteksigolongandarah;
 
 import android.app.Application;
+import android.os.StrictMode;
+
+import com.squareup.leakcanary.LeakCanary;
 
 
 public class MyApp extends Application {
 
     @Override
     public void onCreate() {
-
         super.onCreate();
 
+        if (BuildConfig.DEBUG){
+            if (LeakCanary.isInAnalyzerProcess(this)) {
+                return;
+            }
+            enabledStrictMode();
+            LeakCanary.install(this);
+        }
+    }
+
+    private static void enabledStrictMode() {
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder() //
+                .detectAll() //
+                .penaltyLog() //
+                .penaltyDeath() //
+                .build());
     }
 }
